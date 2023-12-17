a13ea14955c6bfb49d001abc6ecccb560f2bb8bc
John Francis <john9francis@gmail.com>
2023-12-16 18:38:32 -0700

    Added an input from user

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
+use std::io;
+use std::io::Write;
+
 mod basic;
 
 fn main() {
   println!("Hello Math app world!");
+
+  // get input from user
+  let mut input = String::new();
+
+  print!("Input: ");
+  let _ = io::stdout().flush();
+
+  io::stdin().read_line(&mut input).expect("Failed to read line");
+
   let result = basic::add(1.,2.);
   println!("{}", result);
 }
