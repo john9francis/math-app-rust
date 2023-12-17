9dc6fbe8c178f28c2b6d692d2dc2fead194a2ea4
John Francis <john9francis@gmail.com>
2023-12-16 19:41:38 -0700

    Fixed the 'add' function

--- src/basic.rs
+++ src/basic.rs
@@ -1,22 +1,21 @@
-pub fn add(input: &str) -> &str {
-  // convert the input string to integers
-  let mut result = "";
-  
-  let strings : Vec<&str> = input.split_whitespace().collect();
-  let mut floats : Vec<f64> = Vec::new();
+pub fn add(input: &str) -> String {
+  // convert the input string to floats
+  let mut result = String::new();
+  let mut floats: Vec<f64> = Vec::new();
 
-  for s in &strings {
+  for s in input.split_whitespace() {
     if let Ok(number) = s.parse::<f64>() {
-      floats.push(number)
+      floats.push(number);
     } else {
-      result = &format!("{} is not a valid number.", s);
+      result = format!("{} is not a valid number.", s);
+      break;
     }
   }
 
-  if result == "" {
-    &format!("{:?}", floats);
+  if result.is_empty() {
+    let sum: f64 = floats.iter().sum();
+    result = format!("{}", sum);
   }
-  
 
   result
 }
