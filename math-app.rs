5fbb7c6d95a31911cf0febca7d9439ff4e39a637
John Francis <john9francis@gmail.com>
2023-12-16 18:03:05 -0700

    created a simple add function

--- math-app.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-  println!("Hello Math app world!")
-}
---
+++ src/math-app.rs
@@ -0,0 +1,7 @@
+mod add;
+
+fn main() {
+  println!("Hello Math app world!");
+  let result = add::add(1.,2.);
+  println!("{}", result);
+}
